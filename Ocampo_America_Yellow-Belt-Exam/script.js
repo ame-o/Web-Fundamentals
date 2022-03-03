d38fe1aaa8cd9163745e184b3a138762efffb6be
America Ocampo <americavocampo@gmail.com>
2022-03-03 09:02:12 -0800

    changes

--- Ocampo_America_Yellow-Belt-Exam/script.js
+++ Ocampo_America_Yellow-Belt-Exam/script.js
@@ -2,15 +2,15 @@ function remove(element) {
     element.remove();
 }
 
-addEventListener("mouseover",function over(element))
-{
-    var succsdos = document.querySelector('succsdos')
-element.target.querySelector('succsdos')
-}
+// addEventListener("mouseover",function over(succulents))
+// {
+//     var succsdos = document.querySelector('succsdos')
+// succsdos.target.querySelector('succsdos')
+// }
 
-var succsuno = document.querySelector('succsuno')
-function out(element) {
-    element.querySelector(imgStay);
-    console.log(onmouseover.imgStay);
-}
+// var succsuno = document.querySelector('succsuno')
+// function out(succulents) {
+//     succsuno.querySelector(imgStay);
+//     console.log(onmouseover.imgStay);
+// }
 
